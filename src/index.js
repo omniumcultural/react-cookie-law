d19b9a0e8d109840b057026fd1f4b854a4304b57
Tazthor <sprat@omnium.cat>
2020-10-22 10:46:02 +0200

    CookiePanel export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import CookieBanner from './components/CookieBanner';
+import CookiePanel from './components/CookiePanel';
+
 import { isServer } from './helpers';
 
 const CookieBannerUniversal = props => (isServer() ? null : <CookieBanner {...props} />);
+const CookiePanelUniversal = props => (isServer() ? null : <CookiePanel {...props} />);
+
 
 export { CookieBannerUniversal as CookieBanner };
+export { CookiePanelUniversal as CookiePanel };
