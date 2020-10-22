ec5aa8a58fbfcc4d8fcf987584bb03adefc4cc80
rgarcia-cat <rgarcia@omnium.cat>
2020-10-22 13:56:09 +0200

    test new dist with new component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import CookieBanner from './components/CookieBanner';
 import CookiePanel from './components/CookiePanel';
+import CookieBanner2 from './components/CookieBanner2';
 
 
 import { isServer } from './helpers';
 
 const CookieBannerUniversal = props => (isServer() ? null : <CookieBanner {...props} />);
 const CookiePanelUniversal = props => (isServer() ? null : <CookiePanel {...props} />);
+const CookieBannerUniversal2 = props => (isServer() ? null : <CookieBanner2 {...props} />);
 
 export { CookieBannerUniversal as CookieBanner };
 export { CookiePanelUniversal as CookiePanel };
-export { CookieBannerUniversal as CookieBanner2 };
+export { CookieBannerUniversal2 as CookieBanner2 };
