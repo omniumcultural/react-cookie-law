4669e7e302ab3b692f69790bfb5f132adfc6cd5a
rgarcia-cat <rgarcia@omnium.cat>
2020-10-22 12:38:58 +0200

    Test new index export method and update dist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import CookieBanner from './components/CookieBanner';
-import CookiePanel from './components/CookiePanel';
+//import React from 'react';
+//import CookieBanner from './components/CookieBanner';
+//import CookiePanel from './components/CookiePanel';
 
-import { isServer } from './helpers';
-
-const CookieBannerUniversal = props => (isServer() ? null : <CookieBanner {...props} />);
-const CookiePanelUniversal = props => (isServer() ? null : <CookiePanel {...props} />);
+export {default as CookieBanner} from './components/CookieBanner';
+export {default as CookiePanel} from './components/CookiePanel';
 
 
-export { CookieBannerUniversal as CookieBanner, CookiePanelUniversal as CookiePanel };
+//import { isServer } from './helpers';
+
+//const CookieBannerUniversal = props => (isServer() ? null : <CookieBanner {...props} />);
+//const CookiePanelUniversal = props => (isServer() ? null : <CookiePanel {...props} />);
+
+
+//export { CookieBannerUniversal as CookieBanner };
+
