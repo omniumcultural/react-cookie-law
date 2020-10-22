625863ad92f64e2b4ffa29b7311ea541dbc6fa72
rgarcia-cat <rgarcia@omnium.cat>
2020-10-22 13:24:53 +0200

    Fix export issues

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-//import React from 'react';
-//import CookieBanner from './components/CookieBanner';
-//import CookiePanel from './components/CookiePanel';
-export {default as CookieBanner} from './components/CookieBanner';
-export {default as CookieBanner2} from './components/CookieBanner';
-export {default as CookiePanel} from './components/CookiePanel';
+import React from 'react';
+import CookieBanner from './components/CookieBanner';
+import CookiePanel from './components/CookiePanel';
 
 
-//import { isServer } from './helpers';
+import { isServer } from './helpers';
 
-//const CookieBannerUniversal = props => (isServer() ? null : <CookieBanner {...props} />);
-//const CookiePanelUniversal = props => (isServer() ? null : <CookiePanel {...props} />);
+const CookieBannerUniversal = props => (isServer() ? null : <CookieBanner {...props} />);
+const CookiePanelUniversal = props => (isServer() ? null : <CookiePanel {...props} />);
 
-
-//export { CookieBannerUniversal as CookieBanner };
-
+export { CookieBannerUniversal as CookieBanner };
+export { CookiePanelUniversal as CookiePanel };
